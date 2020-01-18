ddd59f3d1ae07e19addf2079adf513e2aa8afebf
George Shao <GeorgeShao123@gmail.com>
2020-01-18 16:55:53 -0500

    Added localfood data

--- app/src/main/java/com/ecoeats/MainActivity.java
+++ app/src/main/java/com/ecoeats/MainActivity.java
@@ -47,10 +47,6 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -62,12 +58,16 @@ public class MainActivity extends AppCompatActivity {
 
         final ListView list = findViewById(R.id.localfood_list);
         ArrayList<SubjectData> arrayList = new ArrayList<SubjectData>();
-        arrayList.add(new SubjectData("JAVA", "https://www.tutorialspoint.com/java/",             "https://www.tutorialspoint.com/java/images/java-mini-logo.jpg"));
-        arrayList.add(new SubjectData("Python", "https://www.tutorialspoint.com/python/", "https://www.tutorialspoint.com/python/images/python-mini.jpg"));
-        arrayList.add(new SubjectData("Javascript", "https://www.tutorialspoint.com/javascript/", "https://www.tutorialspoint.com/javascript/images/javascript-mini-logo.jpg"));
-        arrayList.add(new SubjectData("Cprogramming", "https://www.tutorialspoint.com/cprogramming/", "https://www.tutorialspoint.com/cprogramming/images/c-mini-logo.jpg"));
-        arrayList.add(new SubjectData("Cplusplus", "https://www.tutorialspoint.com/cplusplus/", "https://www.tutorialspoint.com/cplusplus/images/cpp-mini-logo.jpg"));
-        arrayList.add(new SubjectData("Android", "https://www.tutorialspoint.com/android/", "https://www.tutorialspoint.com/android/images/android-mini-logo.jpg"));
+        arrayList.add(new SubjectData("Carrots", "https://www.google.com", "https://1etgc323htj92xdrc5372t71zpf-wpengine.netdna-ssl.com/wp-content/uploads/sites/4/2014/05/January-121x172.jpg"));
+        arrayList.add(new SubjectData("Potatoes", "https://www.google.com/", "https://www.producemarketguide.com/sites/default/files/Commodities.tar/Commodities/potatoes_commodity-page.png"));
+        arrayList.add(new SubjectData("Onions", "https://www.google.com/", "https://ichef.bbci.co.uk/news/410/media/images/80896000/jpg/_80896768_red-onion-think624.jpg"));
+        arrayList.add(new SubjectData("Cucumbers", "https://www.google.com/", "https://www.foodsafetynews.com/files/2018/11/long-English-cucumber-550x311.jpg"));
+        arrayList.add(new SubjectData("Mushrooms", "https://www.google.com/", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtdaWQeAzZuu_C7qFORWQ15LEfFOW_J78h7EZQ7rnT3OJSoEy9&s"));
+        arrayList.add(new SubjectData("Cabbage", "https://www.google.com/", "https://bestoffernearme.com/wp-content/uploads/2019/11/71nFvA-EOeL._SL1500_.jpg"));
+        arrayList.add(new SubjectData("Apples", "https://www.google.com/", "http://thelunchlady.ca/wp-content/uploads/2015/10/Apples-in-your-diet.jpg"));
+        arrayList.add(new SubjectData("Beets", "https://www.google.com/", "https://theforkedspoon.com/wp-content/uploads/2017/04/How-to-Cook-Beets-700x1050.jpg"));
+        arrayList.add(new SubjectData("Parsnips", "https://www.google.com/", "https://www.simplyrecipes.com/wp-content/uploads/2009/12/parsnips-horiz-1800.jpg"));
+        arrayList.add(new SubjectData("Rutabaga", "https://www.google.com/", "http://www.specialtyproduce.com/sppics/658.png"));
         CustomAdapter customAdapter = new CustomAdapter(this, arrayList);
         list.setAdapter(customAdapter);
     }
