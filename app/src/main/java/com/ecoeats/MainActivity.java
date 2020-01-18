4bb51b9827dcc0d9cfff3f694e0487814b75a543
George Shao <GeorgeShao123@gmail.com>
2020-01-18 17:30:40 -0500

    Added more localfood availability data

--- app/src/main/java/com/ecoeats/MainActivity.java
+++ app/src/main/java/com/ecoeats/MainActivity.java
@@ -58,16 +58,25 @@ public class MainActivity extends AppCompatActivity {
 
         final ListView list = findViewById(R.id.localfood_list);
         ArrayList<SubjectData> arrayList = new ArrayList<SubjectData>();
-        arrayList.add(new SubjectData("Carrots", "https://www.google.com", "https://1etgc323htj92xdrc5372t71zpf-wpengine.netdna-ssl.com/wp-content/uploads/sites/4/2014/05/January-121x172.jpg"));
-        arrayList.add(new SubjectData("Potatoes", "https://www.google.com/", "https://www.producemarketguide.com/sites/default/files/Commodities.tar/Commodities/potatoes_commodity-page.png"));
-        arrayList.add(new SubjectData("Onions", "https://www.google.com/", "https://ichef.bbci.co.uk/news/410/media/images/80896000/jpg/_80896768_red-onion-think624.jpg"));
-        arrayList.add(new SubjectData("Cucumbers", "https://www.google.com/", "https://www.foodsafetynews.com/files/2018/11/long-English-cucumber-550x311.jpg"));
-        arrayList.add(new SubjectData("Mushrooms", "https://www.google.com/", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtdaWQeAzZuu_C7qFORWQ15LEfFOW_J78h7EZQ7rnT3OJSoEy9&s"));
-        arrayList.add(new SubjectData("Cabbage", "https://www.google.com/", "https://bestoffernearme.com/wp-content/uploads/2019/11/71nFvA-EOeL._SL1500_.jpg"));
-        arrayList.add(new SubjectData("Apples", "https://www.google.com/", "http://thelunchlady.ca/wp-content/uploads/2015/10/Apples-in-your-diet.jpg"));
-        arrayList.add(new SubjectData("Beets", "https://www.google.com/", "https://theforkedspoon.com/wp-content/uploads/2017/04/How-to-Cook-Beets-700x1050.jpg"));
-        arrayList.add(new SubjectData("Parsnips", "https://www.google.com/", "https://www.simplyrecipes.com/wp-content/uploads/2009/12/parsnips-horiz-1800.jpg"));
-        arrayList.add(new SubjectData("Rutabaga", "https://www.google.com/", "http://www.specialtyproduce.com/sppics/658.png"));
+        arrayList.add(new SubjectData("Carrots", "", "https://1etgc323htj92xdrc5372t71zpf-wpengine.netdna-ssl.com/wp-content/uploads/sites/4/2014/05/January-121x172.jpg"));
+        arrayList.add(new SubjectData("Potatoes", "", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIqMcWxwgu519AXsVIzPeZqZEVGImS_krusw4uRD70bV0IMkVO&s"));
+        arrayList.add(new SubjectData("Onions", "", "https://ichef.bbci.co.uk/news/410/media/images/80896000/jpg/_80896768_red-onion-think624.jpg"));
+        arrayList.add(new SubjectData("Cucumbers", "", "https://www.foodsafetynews.com/files/2018/11/long-English-cucumber-550x311.jpg"));
+        arrayList.add(new SubjectData("Mushrooms", "", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtdaWQeAzZuu_C7qFORWQ15LEfFOW_J78h7EZQ7rnT3OJSoEy9&s"));
+        arrayList.add(new SubjectData("Cabbage", "", "https://bestoffernearme.com/wp-content/uploads/2019/11/71nFvA-EOeL._SL1500_.jpg"));
+        arrayList.add(new SubjectData("Apples", "", "https://i5.walmartimages.com/asr/710231ac-34ab-41a3-9b6c-04d7261ada17_1.a866a242aeccff90ad46e37b7fdf3940.jpeg?odnWidth=450&odnHeight=450&odnBg=ffffff"));
+        arrayList.add(new SubjectData("Beets", "", "https://theforkedspoon.com/wp-content/uploads/2017/04/How-to-Cook-Beets-700x1050.jpg"));
+        arrayList.add(new SubjectData("Parsnips", "", "https://www.simplyrecipes.com/wp-content/uploads/2009/12/parsnips-horiz-1800.jpg"));
+        arrayList.add(new SubjectData("Rutabaga", "", "https://www.farmersalmanac.com/wp-content/uploads/2014/12/turnip-AdobeStock_95185232.jpg"));
+        arrayList.add(new SubjectData("Rhubarb", "", "https://theviewfromgreatisland.com/wp-content/uploads/2019/04/rhubarb-stalks-e1554481252586.jpg"));
+        arrayList.add(new SubjectData("Strawberries", "", "https://theviewfromgreatisland.com/wp-content/uploads/2019/04/rhubarb-stalks-e1554481252586.jpg"));
+        arrayList.add(new SubjectData("Lettuce", "", "https://cfig.ca/wp-content/uploads/2018/11/romaine.jpg"));
+        arrayList.add(new SubjectData("Peppers", "", "https://i5.walmartimages.ca/images/Enlarge/284/684/6000191284684.jpg"));
+        arrayList.add(new SubjectData("Sprouts", "", "https://cdn.drweil.com/wp-content/uploads/2016/12/diet-nutrition_recipes_stir-fried-bean-sprouts_2716x1811_000064913441-600x450.jpg"));
+        arrayList.add(new SubjectData("Squash", "", "https://i0.wp.com/cdn-prod.medicalnewstoday.com/content/images/articles/284/284479/butternut-squash.jpg?w=1155&h=1642"));
+        arrayList.add(new SubjectData("Sweet Potatoes", "", "https://www.thespruceeats.com/thmb/qKLH4scbuqwGursa5IOC172PyOs=/960x0/filters:no_upscale():max_bytes(150000):strip_icc()/sweetpotatoes_getty2400-56a4975c5f9b58b7d0d7b790.jpg"));
+        arrayList.add(new SubjectData("Tomatoes", "", "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2013/5/13/0/FN_tomatoes-thinkstock_s4x3.jpg.rend.hgtvcom.406.305.suffix/1371616351150.jpeg"));
+
         CustomAdapter customAdapter = new CustomAdapter(this, arrayList);
         list.setAdapter(customAdapter);
     }
