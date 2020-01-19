dcb50cba8056edc1556638f4f000ba512e582a1e
S M Jawwad Raza <32301895+S-M-Jawwad-Raza@users.noreply.github.com>
2020-01-18 23:18:33 -0500

    Add Firebase Recipe Pull

--- app/src/main/java/com/ecoeats/Schedule.java
+++ app/src/main/java/com/ecoeats/Schedule.java
@@ -5,16 +5,30 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Schedule extends AppCompatActivity {
+
+    private TextView lunchText;
+    private TextView dinnerText;
+    private ArrayList<Recipe> recipes =  new ArrayList<>();
+    private ArrayList<String> cuisines = new ArrayList<>();
+    private FirebaseFirestore db;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +36,21 @@ public class Schedule extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         navigation.getMenu().getItem(1).setChecked(true);
+
+        db = FirebaseFirestore.getInstance();
+
+        lunchText =  findViewById(R.id.recipe_name_lunch_textview);
+        dinnerText = findViewById(R.id.recipe_name_dinner_textview);
+
+
+        cuisines.add("American");
+        cuisines.add("Italian");
+        cuisines.add("Mediterranean");
+        cuisines.add("South-Asian");
+
+        getRecipes();
+        displaySchedule(0);
+
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -55,4 +84,24 @@ public class Schedule extends AppCompatActivity {
             return false;
         }
     };
+
+    private void displaySchedule(int day){
+        lunchText.setText(recipes.get(day*2).getName());
+        dinnerText.setText(recipes.get(day*2+1).getName());
+    }
+
+    private void getRecipes(){
+        for(String cuisine: cuisines){
+            CollectionReference colref = db.collection(cuisine);
+            colref.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                @Override
+                public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                    List<DocumentSnapshot> dSnaps = queryDocumentSnapshots.getDocuments();
+                    for (DocumentSnapshot dSnap:dSnaps){
+                        recipes.add(dSnap.toObject(Recipe.class));
+                    }
+                }
+            });
+        }
+    }
 }
